ba45b34ad50d8b9740c18b72e1809894ebf50111
Yong Jing <gohyongjing@gmail.com>
2022-12-04 20:14:26 +0800

    Add author and genres

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const {DateTime} = require("luxon");
 const mongoose = require ("mongoose");
 
 const Schema = mongoose.Schema;
@@ -21,6 +22,20 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_birth ?
+  DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+  : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_death ?
+  DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+  : "";
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
